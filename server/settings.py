312ba997d718fa8a6b1acec5bb1fa8f932761ec7
KhadaKayn <marcelo.ponce07@inacapmail.cl>
2026-03-21 19:29:45 -0300

    Cambio a SQLite y ajustes de config

--- server/settings.py
+++ server/settings.py
@@ -93,6 +93,15 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -103,7 +112,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+"""
 
 
 # Password validation
